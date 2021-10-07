3dd2f086e45af0c8ec40bdf5bc34b8dd54f87825
Lukabrazi111 <lukakhangoshvili@gmail.com>
2021-10-08 02:26:49 +0400

    commit updated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
-let start = document.getElementById("start");
+const start = document.getElementById("start");
+const header = document.getElementById("header");
+const box_container = document.getElementById("box-container");
+const arrows = document.getElementById("arrows");
+const logo_start = document.querySelector(".logo_start");
+const arrow_right = document.querySelector(".arrow-right");
+const second_page = document.getElementById("second_page");
+const arrow_left = document.querySelector(".arrow-left");
 
-start.addEventListener("click", function() {
-    document.getElementById("header").style.display = 'flex';
-    document.getElementById("box-container").style.display = 'flex';
-    document.getElementById("arrows").style.display = 'flex';
-    this.parentElement.parentElement.style.display = 'none';
-})
+// Start button
+start.onclick = function () {
+    box_container.classList.add("activeBox");
+    header.classList.add("activeHeader");
+    arrows.classList.add("activeArrows");
+    logo_start.classList.add("hideLogo");
+}
+
+// Next button
+arrow_right.onclick = function () {
+    const num_header = document.querySelector(".num_header").innerText = '2/4';
+    box_container.classList.remove("activeBox");
+    second_page.classList.add("activePage");
+    arrow_left.classList.add("activeArrowLeft");
+}
