d575fa94bbf8c5d3a0f5bb26a55eb8742930ada4
Lukabrazi111 <lukakhangoshvili@gmail.com>
2021-10-08 18:21:01 +0400

    created pages(2,3)

--- js/app.js
+++ js/app.js
@@ -6,19 +6,59 @@ const logo_start = document.querySelector(".logo_start");
 const arrow_right = document.querySelector(".arrow-right");
 const second_page = document.getElementById("second_page");
 const arrow_left = document.querySelector(".arrow-left");
+const slide = document.querySelectorAll(".slide");
 
 // Start button
-start.onclick = function () {
-    box_container.classList.add("activeBox");
-    header.classList.add("activeHeader");
-    arrows.classList.add("activeArrows");
-    logo_start.classList.add("hideLogo");
+// start.addEventListener("click", function() {
+//     box_container.classList.add("activeBox");
+//     header.classList.add("activeHeader");
+//     arrows.classList.add("activeArrows");
+//     logo_start.classList.add("hideLogo");
+// });
+
+let current = 0;
+
+function reset() {
+    for(let i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
 }
 
-// Next button
-arrow_right.onclick = function () {
-    const num_header = document.querySelector(".num_header").innerText = '2/4';
-    box_container.classList.remove("activeBox");
-    second_page.classList.add("activePage");
-    arrow_left.classList.add("activeArrowLeft");
+function startSlide() {
+    reset();
+    slide[0].style.display = "flex";
 }
+
+function slideRight() {
+    reset();
+    slide[current + 1].style.display = "flex";
+    current++;
+}
+
+arrow_right.addEventListener("click", function() {
+    slideRight();
+})
+
+startSlide();
+
+
+
+
+// Next button
+// arrow_right.onclick = function () {
+//     const num_header = document.querySelector(".num_header").innerText = '2/4';
+//     box_container.classList.remove("activeBox");
+//     second_page.classList.add("activePage");
+//     arrow_left.classList.add("activeArrowLeft");
+// }
+//
+// arrow_left.onclick = function () {
+//     box_container.classList.add("activeBox");
+//     header.classList.add("activeHeader");
+//     arrows.classList.add("activeArrows");
+//     logo_start.classList.add("hideLogo");
+//
+//     document.querySelector(".num_header").innerText = '1/4'
+//     second_page.classList.remove("activePage");
+//     arrow_left.classList.remove("activeArrowLeft");
+// }
